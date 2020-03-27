51f6718efd867d57558db8b89db11775cedb369c
draganov <draganov89@mail.bg>
2020-03-27 23:08:21 +0200

    New model added - CreateAds

---
+++ HireEachOther/HireEachOther/Pages/CreateAds.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HireEachOther.Pages
+{
+    [Authorize]
+    public class CreateAdsModel : PageModel
+    {
+        public string Ad { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
